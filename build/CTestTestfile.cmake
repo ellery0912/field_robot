7e12273bad4a56b870c42a22b01efbede1e4d47e
ellery0912 <55952190+ellery0912@users.noreply.github.com>
2020-11-19 14:20:13 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/funway/field_robot/src
+# Build directory: /home/funway/field_robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("follw_wall")
+subdirs("rplidar_ros")
